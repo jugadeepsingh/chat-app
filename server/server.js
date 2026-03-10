d725723f7d7c4ac42290c4a20badb363791ebd63
Jugadeep Singh <jugadeepsingh@gmail.com>
2026-03-10 12:33:02 +0530

    fix homepage route

--- server/server.js
+++ server/server.js
@@ -58,3 +58,7 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("Chat App Backend Running");
+});
