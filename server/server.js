7a88b82810a7c952cfdfec79782367618c5dcb24
Jugadeep Singh <jugadeepsingh@gmail.com>
2026-03-11 13:05:24 +0530

    prepare project for render deployment

--- server/server.js
+++ server/server.js
@@ -2,51 +2,82 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const http = require("http");
-const path = require("path");
 const { Server } = require("socket.io");
-const { connectDB } = require("./config/db");
+const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
 connectDB();
 
-// Routes
+const app = express();
+
+const CLIENT_URL = process.env.CLIENT_URL || "*";
+
+app.use(
+  cors({
+    origin: CLIENT_URL === "*" ? true : CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+// API routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/chats", require("./routes/chatRoutes"));
 app.use("/api/messages", require("./routes/messageRoutes"));
 
-// Serve React frontend in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
-  );
-}
+// Serve frontend build
+const clientBuildPath = path.join(__dirname, "../client/build");
+app.use(express.static(clientBuildPath));
 
+app.get("/api", (req, res) => {
+  res.send("Chat App API is running...");
+});
+
+// For React routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientBuildPath, "index.html"));
+});
+
+// HTTP + Socket.io server
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*" },
+  cors: {
+    origin: CLIENT_URL === "*" ? true : CLIENT_URL,
+    methods: ["GET", "POST"],
+  },
+  pingTimeout: 60000,
 });
 
 io.on("connection", (socket) => {
+  console.log("Socket connected:", socket.id);
+
   socket.on("setup", (userData) => {
     socket.join(userData._id);
     socket.emit("connected");
   });
-  socket.on("join chat", (room) => socket.join(room));
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User joined room:", room);
+  });
+
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-  socket.on("new message", (newMessage) => {
-    const chat = newMessage.chat;
-    if (!chat.users) return;
+
+  socket.on("new message", (newMessageReceived) => {
+    const chat = newMessageReceived.chat;
+    if (!chat.users) return console.log("chat.users not defined");
+
     chat.users.forEach((user) => {
-      if (user._id === newMessage.sender._id) return;
-      socket.in(user._id).emit("message received", newMessage);
+      if (user._id === newMessageReceived.sender._id) return;
+      socket.in(user._id).emit("message received", newMessageReceived);
     });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected:", socket.id);
   });
 });
 
