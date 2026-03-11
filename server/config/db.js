556d8dac2c05810aa31c0d236492b3f7aa9639f5
Jugadeep Singh <jugadeepsingh@gmail.com>
2026-03-12 02:26:16 +0530

    fix Sequelize import

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,17 @@
+const { Sequelize } = require("sequelize");
+
 const sequelize = new Sequelize(process.env.MYSQL_PUBLIC_URL, {
-  dialect: 'mysql',
+  dialect: "mysql",
   logging: false,
 });
-
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync({ alter: true });
-    console.log('MySQL connected ✅');
+    console.log("MySQL connected ✅");
   } catch (err) {
-    console.error('MySQL connection error:', err);
+    console.error("MySQL connection error:", err);
     process.exit(1);
   }
 };
