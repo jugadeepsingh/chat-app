fa55154806680dfd2141fd77e309030bc9e35e51
Jugadeep Singh <jugadeepsingh@gmail.com>
2026-03-11 11:15:53 +0530

    fix all routes and middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,16 +1,15 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const { User } = require("../models");
 
 const protect = async (req, res, next) => {
   let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select("-password");
+      req.user = await User.findByPk(decoded.id, {
+        attributes: { exclude: ["password"] },
+      });
       next();
     } catch (error) {
       res.status(401).json({ message: "Not authorized, token failed" });
--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {
-  accessChat,
-  fetchChats,
-  createGroupChat,
-  renameGroup,
-  addToGroup,
-  removeFromGroup,
-} = require("../controllers/chatController");
+const { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup } = require("../controllers/chatController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", protect, accessChat);
